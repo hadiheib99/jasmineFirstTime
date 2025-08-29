8cdb27d3b552ba67e42d69f04c9bf9300de7a035
Abdelhadi Heib <hadiheib99@gmail.com>
2025-08-29 14:02:32 +0300

    first commit

---
+++ spec/calculator.spec.ts
@@ -0,0 +1,11 @@
+import { add, multiply } from "../src/calculator";
+
+describe("Calculator", () => {
+  it("should add two numbers correctly", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  it("should multiply two numbers correctly", () => {
+    expect(multiply(2, 1)).toBe(6);
+  });
+});
